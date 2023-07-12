a7221b3b032b5168f1c3b2ab697a6636846463e7
jaydeep <jaydeepk@gmail.com>
2023-07-12 16:19:34 +0530

    Added spring actuator.

--- src/test/kotlin/com/component/orders/contract/ContractTests.kt
+++ src/test/kotlin/com/component/orders/contract/ContractTests.kt
@@ -19,12 +19,14 @@ class ContractTests : SpecmaticJUnitSupport() {
         private const val SPECMATIC_TEST_PORT = "8080"
         private const val SPECMATIC_STUB_HOST = "localhost"
         private const val SPECMATIC_STUB_PORT = 9000
+        private const val ACTUTATOR_MAPPINGS_ENDPOINT = "http://$SPECMATIC_TEST_HOST:$SPECMATIC_TEST_PORT/actuator/mappings"
 
         @JvmStatic
         @BeforeAll
         fun setUp() {
             System.setProperty("host", SPECMATIC_TEST_HOST)
             System.setProperty("port", SPECMATIC_TEST_PORT)
+            System.setProperty("endpointsAPI", ACTUTATOR_MAPPINGS_ENDPOINT)
 
             stub = createStub(SPECMATIC_STUB_HOST, SPECMATIC_STUB_PORT)
 
