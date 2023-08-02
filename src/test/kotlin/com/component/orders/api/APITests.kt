c2d7d96a1232adea0e9cfcf0ea26f7c9902ecbec
jaydeep <jaydeepk@gmail.com>
2023-08-02 14:57:09 +0530

    Shifted Kafka verification to teardown. Extracted http host port and kafka port constants, added descriptions in comments. Upgraded to Specmatic Core version 0.73.0

--- src/test/kotlin/com/component/orders/api/APITests.kt
+++ src/test/kotlin/com/component/orders/api/APITests.kt
@@ -9,7 +9,6 @@ import `in`.specmatic.stub.createStub
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ConfigurableApplicationContext
 
@@ -18,35 +17,44 @@ class APITests {
     fun apiTests(): Karate {
         return Karate().path("apiTests.feature").relativeTo(this::class.java)
     }
-
-    @Test
-    fun `test expectations set on the kafka mock are met`() {
-        kafkaMock.awaitMessages(3)
-        val result = kafkaMock.verifyExpectations()
-        assertThat(result.success).isTrue
-        assertThat(result.errors).isEmpty()
-    }
-
     companion object {
-        private var service: ConfigurableApplicationContext? = null
-        private lateinit var stub: ContractStub
+        private lateinit var context: ConfigurableApplicationContext
+        private lateinit var httpStub: ContractStub
         private lateinit var kafkaMock: KafkaMock
+        private const val HTTP_STUB_HOST = "localhost"
+        private const val HTTP_STUB_PORT = 9000
+        private const val KAFKA_MOCK_PORT = 9092
+        private const val EXPECTED_NUMBER_OF_MESSAGES = 3
 
         @BeforeAll
         @JvmStatic
         fun setUp() {
-            stub = createStub()
-            kafkaMock = KafkaMock.create()
+            // Start Specmatic Http Stub
+            httpStub = createStub(HTTP_STUB_HOST, HTTP_STUB_PORT)
+
+            // Start Specmatic Kafka Mock and set the expectations
+            kafkaMock = KafkaMock.create(KAFKA_MOCK_PORT)
             kafkaMock.start()
-            kafkaMock.setExpectations(listOf(Expectation("product-queries", 3)))
-            service = SpringApplication.run(Application::class.java)
+            kafkaMock.setExpectations(listOf(Expectation("product-queries", EXPECTED_NUMBER_OF_MESSAGES)))
+
+            // Start Springboot application
+            context = SpringApplication.run(Application::class.java)
         }
 
         @AfterAll
         @JvmStatic
         fun tearDown() {
-            service?.close()
-            stub.close()
+            // Shutdown Springboot application
+            context.close()
+
+            // Shutdown Specmatic Http Stub
+            httpStub.close()
+
+            // Verify Specmatic Kafka mock and shutdown
+            kafkaMock.awaitMessages(EXPECTED_NUMBER_OF_MESSAGES)
+            val result = kafkaMock.verifyExpectations()
+            assertThat(result.success).isTrue
+            assertThat(result.errors).isEmpty()
             kafkaMock.close()
             // Wait for Kafka server to stop
             Thread.sleep(15000)
