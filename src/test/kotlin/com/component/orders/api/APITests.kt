206e918430b69f423058242269d625425e09c4dc
Hari Krishnan <harikrishnan83@gmail.com>
2023-11-27 19:59:29 +0530

    upgrading to JDK 17, spring-boot 3.2.0 and configurations to exclude slf4j to avoid conflict with logback

--- src/test/kotlin/com/component/orders/api/APITests.kt
+++ src/test/kotlin/com/component/orders/api/APITests.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ConfigurableApplicationContext
 
-
 class APITests {
 
     @Karate.Test
