52cec1b350774de51f04ae1f780019df3e9a4a0e
Naresh Jain <nashjain@gmail.com>
2023-11-30 04:00:53 +1100

    Added response code for 404 in GlobalExceptionHandler

--- src/main/kotlin/com/component/orders/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/component/orders/handlers/GlobalExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.client.ResourceAccessException
+import org.springframework.web.servlet.resource.NoResourceFoundException
 import java.time.LocalDateTime
 
 @ControllerAdvice
@@ -12,9 +13,11 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception::class)
     fun handleGenericException(ex: Exception): ResponseEntity<ErrorResponse> {
-        var badRequest = HttpStatus.BAD_REQUEST
-        if (ex is ResourceAccessException)
-            badRequest = HttpStatus.SERVICE_UNAVAILABLE
+        val badRequest = when (ex) {
+            is ResourceAccessException -> HttpStatus.SERVICE_UNAVAILABLE
+            is NoResourceFoundException -> HttpStatus.NOT_FOUND
+            else -> HttpStatus.BAD_REQUEST
+        }
         val errorResponse = ErrorResponse(
             LocalDateTime.now(),
             badRequest.value(),
